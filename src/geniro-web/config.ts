2348c43c628c8740f90a85c34aa03276550d7ede
Mattéo Delabre <git.matteo@delab.re>
2025-10-21 18:14:10 -0400

    uris: Change prefix to use http://

--- src/geniro-web/config.ts
+++ src/geniro-web/config.ts
@@ -1,4 +1,4 @@
 export const webRoot = "http://localhost:8765";
 export const listenPort = 8765;
 export const databaseEndpoint = "http://localhost:7200/repositories/geniro";
-export const mainRdfNamespace = "https://diro.umontreal.ca/geniro";
+export const mainRdfNamespace = "http://diro.umontreal.ca/geniro";
