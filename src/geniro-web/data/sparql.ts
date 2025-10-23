53d83be0938c7ea2457b9da788bc2003e4a26aac
Mattéo Delabre <git.matteo@delab.re>
2025-10-22 22:13:02 -0400

    sparql: Add getObjects

--- src/geniro-web/data/sparql.ts
+++ src/geniro-web/data/sparql.ts
@@ -1,4 +1,5 @@
 import rdf from "@rdfjs/data-model";
+import toNT from "@rdfjs/to-ntriples";
 import namespace from "@rdfjs/namespace";
 import { SparqlJsonParser } from "sparqljson-parse";
 import { Node } from "rdf-sparql-builder/lib/Node";
@@ -59,5 +60,37 @@ export const update = async (
 
     if (!res.ok) {
         throw new Error(`Invalid server response: ${res.statusText} (${res.status})`);
+    }
+};
+
+export const getObjects = async (
+    endpoint: string,
+    subject: Node,
+    predicate: Node,
+): Promise<any[]> => {
+    console.log("getting object:", toNT(subject), toNT(predicate));
+
+    const url = new URL(endpoint + "/statements");
+    url.search = new URLSearchParams({
+        "subj": toNT(subject),
+        "pred": toNT(predicate),
+    });
+
+    const res = await fetch(url, {
+        method: "GET",
+        headers: { "accept": "application/rdf+json" },
+    });
+
+    if (!res.ok) {
+        throw new Error(`Invalid server response: ${res.statusText} (${res.status})`);
+    }
+
+    const data = await res.text();
+
+    try {
+        const parsed = JSON.parse(data);
+        return parsed[subject.value]?.[predicate.value];
+    } catch (_) {
+        throw new Error("Invalid server response: " + data);
     }
 };
