9a9d0644a13845610caac8b58acb38d0598cddbe
Mattéo Delabre <git.matteo@delab.re>
2026-03-28 12:24:20 -0400

    Ignore config file, create template config instead

--- src/geniro-web/config.sample.ts
+++ src/geniro-web/config.sample.ts
@@ -11,11 +11,18 @@ export const databaseEndpoint = "http://localhost:7200/repositories/geniro";
 export const mainRdfNamespace = "http://diro.umontreal.ca/geniro";
 
 /** Contact name and email sent to servers to identify the data requests. */
-export const adminName = "Mattéo Delabre";
-export const adminEmail = "geniro.matteo@delab.re";
+export const adminName = "Admin Name";
+export const adminEmail = "admin.name@example.org";
 
 /** Minimum delay between two data requests per domain, in milliseconds. */
 export const minRequestDelay = 2500;
 
 /** Maximum number of attempts for a data request before giving up. */
 export const maxRequestAttempts = 3;
+
+/** Minimum time to wait before refreshing a record from MathGenealogy, in seconds. */
+export const mathgenRefreshDelay = 7 * 24 * 60 * 60;
+
+/** MathGenealogy API login and password */
+export const mathgenLogin = "";
+export const mathgenPassword = "";
